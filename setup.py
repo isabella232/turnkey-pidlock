4a9d3ca2a81e03a0c65d84572ef9eaa504d0ce53
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 03:17:01 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-pidlock',
+    version='1.0',
+    description='A small filelocking library',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-pidlock',
+    packages=['pidlock'])
